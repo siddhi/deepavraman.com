9e783874aacc7de2bb2129a6c63db99ac2787b87
Siddharta Govindaraj <siddharta@silverstripesoftware.in>
2024-01-04 16:58:08 +0530

    Initial commit of website

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Deepa Venkatraman'
+SITENAME = 'Deepa Venkatraman'
+SITEURL = "https://www.deepavraman.com"
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'en'
+THEME = 'theme/spotlight'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = tuple()
+
+# Social widget
+SOCIAL = tuple()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
