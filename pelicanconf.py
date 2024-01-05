fe043527c739f97696cfba9f797073b84eebfa3b
Siddharta Govindaraj <siddharta@silverstripesoftware.in>
2024-01-05 17:18:56 +0530

    Changed pagination to 5

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,7 +22,7 @@ LINKS = tuple()
 # Social widget
 SOCIAL = tuple()
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
