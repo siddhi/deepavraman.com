ef507cae19ffbe27556fb6b9abee05bf43839f14
Siddharta Govindaraj <siddharta@silverstripesoftware.in>
2024-01-05 17:18:34 +0530

    Added tailwind build process

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ["./output/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ]
+}
